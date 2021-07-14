58299e9f7eb557aa07b94bb030cb9ac9a47611ea
Jordan Mellow <jrdmellow@gmail.com>
2021-07-13 20:26:51 -0400

    Version 0.2.6

--- ESOPie/ESOPie_Globals.lua
+++ ESOPie/ESOPie_Globals.lua
@@ -1,7 +1,7 @@
 local L = GetString
 ESOPie = ESOPie or {}
 ESOPie.name = "ESOPie"
-ESOPie.version = "0.2.5 BETA"
+ESOPie.version = "0.2.6 BETA"
 ESOPie.author = "FiveStar"
 ESOPie.url = "https://github.com/jrdmellow/ESOPie/wiki"
 ESOPie.slashCommand = "/esopie"
